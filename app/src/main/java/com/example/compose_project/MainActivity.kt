f1d4162dd6f90c521eb2e1be1c11bedc5109f74b
vudung8320 <vudung8320@gmail.com>
2024-03-27 11:57:01 +0700

    Row and Column

--- app/src/main/java/com/example/compose_project/MainActivity.kt
+++ app/src/main/java/com/example/compose_project/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.compose_project
 
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_project.ui.theme.Compose_projectTheme
-import com.example.compose_project.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +28,32 @@ class MainActivity : ComponentActivity() {
         setContent {
             Compose_projectTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    // Row
+                    Row(
+                        modifier = Modifier
+                            .height(500.dp)
+                            .width(500.dp)
+                            .background(Color.LightGray),
+                            horizontalArrangement = Arrangement.Start
 
-                    Column {
-                        Greeting("Android")
-                        CustomText(text = "Meomeo")
+                    ) {
+                        CustomItem(weight = 1f, color = MaterialTheme.colorScheme.error)
+                        CustomItem(weight = 3f)
+                    }
+
+                    // Column
+                    Column(
+                        modifier = Modifier
+                            .height(500.dp)
+                            .width(500.dp)
+                            .background(Color.LightGray),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CustomItem(weight = 1f, color = MaterialTheme.colorScheme.error)
+                        CustomItem(weight = 3f)
                     }
                 }
             }
@@ -34,29 +61,47 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Row Compose
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colorScheme.primary) {
+    Surface(
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .weight(weight),
+        color = color
+    ) {}
 }
 
+// Column compose
 @Composable
-fun CustomText(text: String) {
-    Text(
-        text = text,
-        style = Typography.titleSmall
-    )
+fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colorScheme.primary) {
+    Surface(
+        modifier = Modifier
+            .width(50.dp)
+            .weight(weight),
+        color = color
+    ) {}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_projectTheme {
-        Column {
-            Greeting("Android")
-            CustomText(text = "Meomeo")
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            CustomItem(weight = 1f, color = MaterialTheme.colorScheme.error)
+            CustomItem(weight = 3f)
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CustomItem(weight = 1f, color = MaterialTheme.colorScheme.error)
+            CustomItem(weight = 3f)
         }
     }
 }
